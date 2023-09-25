9880e2c6df63db33feda5e236470f5980faac1c1
Juan Ramírez <artjjrn@gmail.com>
2023-09-25 17:18:16 -0300

    Remove express routes

--- src/rest/folder/:param/post.ts
+++
@@ -1,8 +0,0 @@
-import type { Response } from 'express';
-import type { ExtendedRequest } from '@type/request';
-
-const handler = (req: ExtendedRequest, res: Response) => {
-  return res.status(200).json({ message: 'Test POST responde' });
-};
-
-export default handler;
