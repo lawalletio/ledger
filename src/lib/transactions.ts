fcb2b498a54ff42c9a6c1e562158e96c5f9d840b
Juan José Ramírez <artjjrn@gmail.com>
2024-03-21 10:46:17 -0300

    Whitelist extra tags for response messages

--- src/lib/transactions.ts
+++ src/lib/transactions.ts
@@ -122,9 +122,7 @@ export function getTxHandler(
       receiverId: nostrEvent.tags.filter((t) => t[0] == 'p')[1][1],
       eventId: event.id,
       content: event.payload,
-      extraTags: nostrEvent.tags.filter(
-        (t) => !['p', 't', 'delegation'].includes(t[0]),
-      ),
+      extraTags: nostrEvent.tags.filter((t) => ['e', 'bolt11'].includes(t[0])),
     };
     if (undefined === event.payload) {
       log('Unable to parse content for %s', event.id);
